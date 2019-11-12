79aaef6cad994d965a458e1af9887ae0e4441bcf
fjaviersaezm <fjaviersaezm@gmail.com>
2019-11-12 19:05:25 +0100

    Added gallery and autoshare

--- Gps/app/src/main/java/com/example/gps/MainActivity.kt
+++ Gps/app/src/main/java/com/example/gps/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.gps
 
 import android.Manifest
 import android.annotation.SuppressLint
+import android.bluetooth.BluetoothAdapter
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
@@ -17,24 +18,96 @@ import androidx.core.app.ActivityCompat
 import com.google.android.gms.location.*
 import kotlin.math.sqrt
 import android.util.Log
+import android.widget.Button;
+import android.graphics.Bitmap
+import android.provider.MediaStore
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.R.attr.name
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import android.R.attr.name
+import android.app.Activity
+import android.database.Cursor
+import android.os.Build
+import android.widget.ImageView
+import java.io.IOException
 
 
 private val TAG: String = MainActivity::class.java.simpleName
 
 class MainActivity : AppCompatActivity() {
 
+    //Vals
     val PERMISSION_ID = 42
+    private val GALLERY = 1
+    private val SHARE   = 2
+    private val RESULT_LOAD_IMG = 1;
     lateinit var mFusedLocationClient: FusedLocationProviderClient
+    lateinit var bluetoothAdapter: BluetoothAdapter
+
+    //button: to delete
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if(!checkPermissionsData())
+            requestPermissionsData()
 
+        //Activate location services
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
 
+        // get button. will be deleted
+        val button = findViewById<Button>(R.id.galBut)
+        button?.setOnClickListener {
 
+            choosePhotoFromGallery()
+
+        }
+
+
+        //Activate bluetooth
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter?.isEnabled == false) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            val REQUEST_ENABLE_BT = 1
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+        }
+
+
+        //Get location
         getLastLocation()
     }
+
+    private fun choosePhotoFromGallery() {
+
+            val intent = Intent(Intent.ACTION_PICK)
+            intent.type = "image/*"
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivityForResult(intent, GALLERY)
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK && requestCode ==  GALLERY){
+
+            val image = data?.data
+            val sharingIntent = Intent(Intent.ACTION_SEND);
+            sharingIntent.addFlags((Intent.FLAG_ACTIVITY_CLEAR_WHEN_TASK_RESET))
+		    sharingIntent.setType("image/*");
+		    sharingIntent.putExtra(Intent.EXTRA_STREAM, image);
+		    startActivity(Intent.createChooser(sharingIntent, "Share Image Using"));
+
+        }
+
+    }
+
+
 
     @SuppressLint("MissingPermission")
     private fun getLastLocation() {
@@ -109,6 +182,16 @@ class MainActivity : AppCompatActivity() {
         return false
     }
 
+    private fun checkPermissionsData(): Boolean {
+        if (ActivityCompat.checkSelfPermission(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
     private fun requestPermissions() {
         ActivityCompat.requestPermissions(
             this,
@@ -116,6 +199,14 @@ class MainActivity : AppCompatActivity() {
             PERMISSION_ID
         )
     }
+    private fun requestPermissionsData() {
+        ActivityCompat.requestPermissions(
+            this,
+            arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE),
+            PERMISSION_ID
+        )
+    }
+
 
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
@@ -163,5 +254,7 @@ class MainActivity : AppCompatActivity() {
 
         return Monument("No monument",0.0,0.0)
     }
+
+
 
 }
